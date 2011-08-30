13fe62b72d36657082eb63ee7067453b20adfeef
John Gietzen <otac0n@gietzen.us>
2011-08-29 20:20:07 -0500

    Added file discovery.

--- LogImporter/Program.cs
+++ LogImporter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,15 @@ namespace LogImporter
     {
         static void Main(string[] args)
         {
+            var files = DiscoverFiles();
+        }
+
+        /// <summary>
+        /// Enumerates CSV files in the current directory.
+        /// </summary>
+        private static IEnumerable<string> DiscoverFiles()
+        {
+            return Directory.EnumerateFiles("*.csv").Select(f => Path.GetFileName(f));
         }
     }
 }
