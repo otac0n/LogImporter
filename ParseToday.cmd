0a1dd1b5ca207159a0e9ea1451c1eba227b400b2
John Gietzen <otac0n@gietzen.us>
2011-09-29 09:26:08 -0500

    Set up quoting for results in the CSV output script, to prevent certain fields from being guessed as numbers.

--- ParseToday.cmd
+++ ParseToday.cmd
@@ -4,4 +4,4 @@ set logparser="C:\Program Files\Log Parser 2.2\LogParser.exe"
 set siteid=%1 
 if "%siteid%"=="" set siteid=1 
 set logdate=%date:~-2%%date:~4,2%%date:~7,2% 
-%logparser% "SELECT * INTO log-w3svc%siteid%-ex%logdate%.csv FROM C:\WINDOWS\system32\LogFiles\W3SVC%siteid%\ex%logdate%.log"
+%logparser% "SELECT * INTO log-w3svc%siteid%-ex%logdate%.csv FROM C:\WINDOWS\system32\LogFiles\W3SVC%siteid%\ex%logdate%.log" -oDQuote:ON
