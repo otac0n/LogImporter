ad1836d4a4a83ab4d02042a34708057a6439b9d6
John Gietzen <otac0n@gietzen.us>
2012-06-04 15:37:58 -0700

    Fixed the output option 'oDQuotes'.

--- ParseToday.cmd
+++ ParseToday.cmd
@@ -1,7 +1,7 @@
 @echo off
-set logparser="C:\Program Files\Log Parser 2.2\LogParser.exe"
+set logparser="%programfiles(x86)%\Log Parser 2.2\LogParser.exe"
 
 set siteid=%1
 if "%siteid%"=="" set siteid=1
 set logdate=%date:~-2%%date:~4,2%%date:~7,2%
-%logparser% "SELECT * INTO log-w3svc%siteid%-ex%logdate%.csv FROM C:\WINDOWS\system32\LogFiles\W3SVC%siteid%\ex%logdate%.log" -oDQuote:ON
+%logparser% "SELECT * INTO log-w3svc%siteid%-ex%logdate%.csv FROM C:\WINDOWS\system32\LogFiles\W3SVC%siteid%\ex%logdate%.log" -oDQuotes:ON
